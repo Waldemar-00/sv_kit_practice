76c8179a4888924a5971ee8e4e0baa597defe83c
uladzimir <01myit@gmail.com>
2025-06-05 21:28:37 +0300

    Getter and setter for derived. Gettre and setter in class and derived values.

---
+++ kit-app/src/routes/derived/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import '../../styles.css'
+    let total = 200
+    let spent = $state(0)
+
+    let left = {
+        get rest() {
+            return total - spent
+        },
+        set rest(s) {
+            spent = s
+        }
+    }
+</script>
+
+<div class=wrapper>
+    <h1>Derived without side effects</h1>
+    <label title="spent line">
+         spent <input type="range" max={total} bind:value={spent}>
+    </label>
+
+    <hr>
+    <label title="total line">
+         total <input type="range" max={total} bind:value={left.rest}>
+    </label>
+</div>
+
+<style>
+ input {
+    vertical-align: middle;
+ }
+</style>
---
+++ kit-app/src/routes/temperature/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+    import '../../styles.css'
+    class Temperature {
+        #celsius = 0
+        #fahrenheit = 0
+        get celsius(){return this.#celsius}
+        set celsius(c){
+            this.#celsius = c
+            this.#fahrenheit = c * 9/5 + 32
+        }
+        get fahrenheit() {
+            return this.#fahrenheit
+        }
+        set fahrenheit(f) {
+            this.#fahrenheit = f
+            this.#celsius = (f - 32) * 5/9
+        }
+    }
+    const temperature = new Temperature()
+</script>
+<div class="wrapper">
+    Celsius: <input type="number" bind:value={temperature.celsius}>
+    <hr>
+    Fahrenheit: <input type="number" bind:value={temperature.fahrenheit}>
+</div>
+<style>
+    input {
+        padding: 0.7rem 1.5rem;
+    }
+</style>
