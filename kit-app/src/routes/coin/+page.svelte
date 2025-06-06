dfc9695122099156de99986fff07c9beab1a038b
uladzimir <01myit@gmail.com>
2025-06-06 21:05:36 +0300

    Coin flip via class.

---
+++ kit-app/src/routes/coin/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+   import '../../styles.css'
+   // @ts-ignore
+   class Coin {
+      side = $state()
+      // @ts-ignore
+      history = $state([])
+      constructor() {
+         this.flip = this.flip.bind(this)
+      }
+      flip() {
+         this.side = Math.random() < 0.5 ? 'HEAD' : 'TAIL'
+         this.history.push(this.side)
+      }
+
+   }
+const coin = new Coin()
+</script>
+<h1>Coin side: {coin.side}</h1>
+<h2>Coin history: {coin.history}</h2>
+<button onclick={coin.flip}>
+   flip me
+</button>
