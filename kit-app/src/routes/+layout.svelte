858c4dc6f054f729ce6bb471a61d8632d670b9ed
uladzimir <01myit@gmail.com>
2025-06-07 17:23:53 +0300

    Layout via select and option tags.

---
+++ kit-app/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { goto } from '$app/navigation';
+
+    let {children} = $props()
+    let selected = $state('')
+    const anchors = [{id: crypto.randomUUID(), title:''}, {id: crypto.randomUUID(), title:'bindable'}, {id: crypto.randomUUID(), title:'coin'}, {id: crypto.randomUUID(), title:'counter'}, {id: crypto.randomUUID(), title:'derived'}, {id: crypto.randomUUID(), title:'effect'}, {id: crypto.randomUUID(), title:'temperature'}]
+    $effect(() => {
+        goto(selected)
+    })
+    const sel = {
+        get sel() {
+            return selected ? selected : '/'
+        },
+        set sel(s) {
+            selected = s
+        }
+    }
+</script>
+<select bind:value={sel.sel}>
+{#each anchors as anchor (anchor.id)}
+    <option value={`/${anchor.title}`} selected={anchor.title ? false : true}>{anchor.title ? anchor.title.toLocaleUpperCase() : 'HOME'}</option>
+{/each}
+</select>
+{@render children()}
